cdbf9217a8529bf95664daae112d93184e845c1a
zena-stevenson <the.z.stevenson@gmail.com>
2022-10-11 14:43:11 -0600

    integrate into the djangoverse, plus saving to db
    re: integration with django: this script now runs in that environment and accesses the db correctly. I'll eventually make this whole thing a command that runs from manage.py.
    
    re: saving new context products to the db: I now know how to do it and have the test code in here, but might not do much with it until we've made some more decisions about changing up the context section of the website. What this code needs to put into what tables here is connected to how the user does it on the website, and how the user does context on the website is (i think / hope) going to be changed and improved fairly soon, so,

--- new_populate_context.py
+++ new_populate_context.py
@@ -1,13 +1,22 @@
-#these both come built in with python 3- no harassing ICT to install stuff for us, yay
 import json
 import urllib.request
+
+#important block for running extra scripts in the command line like this
+#(I'll refactor this whole thing into a management command later)
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elsa.settings')
+import django
+django.setup()
+########################
+
+import build.models
 
 def fetch_ids(type):
     '''Fetches logical identifiers from context products' xml labels, thru the 
     PDS context search API. Give it a context product type (i.e. 'Target') and 
-    this will fetch a list of all targets (and then presumably we'll compare 
-    them to what's in the database already, so we can update and add as 
-    necessary).
+    this will fetch a list of all products with that type (and then presumably 
+    we'll compare them to what's in the database already, so we can update and
+    add as necessary).
     Note that type should be a string, and capitalized- Target, Mission, etc. 
     Returns a list of LID's.
 
@@ -28,12 +37,21 @@ class Context_Product:
 
         self.lid = lid
         self.type=self.lid.split(':')[4]
-        self.context_info=self.get_keyword_dict(self.type)
+        self.get_keyword_dict(self.type)
+        self.get_model_name(self.type)
     
+    #these 'get' functions are separate just to avoid clogging up __init__ 
+    # with a bunch of huge dictionaries
     def get_keyword_dict(self, type):
         '''Gets the right keywords we need for a given type of
-         context product, since we need different info for different products. 
-         Still in the process of building this one'''
+         context product, since we need different info for different products.
+
+         Still in the process of building this one. Several of these keywords
+         aren't currently in use in the database (they all have to do with 
+         references to other context products), but I'm fetching them anyway
+         because I think the site and db will be better if we add them
+         later. Connections between context products being the bones of the
+         whole pds4 philosophy and all that.'''
 
         big_keyword_dict={'target':{'identifier':None, 'target_name':None, 'target_type':None, 'version_id':None } ,
                           'investigation':{'identifier':None, 'investigation_name':None, 'investigation_type':None, 'version_id':None, 'instrument_host_ref':None, 'target_ref':None }, 
@@ -42,11 +60,15 @@ class Context_Product:
                           'instrument_host': {'identifier':None, 'instrument_host_name':None, 'instrument_host_type':None, 'version_id':None, 'investigation_ref':None, 'target_ref':None, 'instrument_ref':None },
                           'facility':{'identifier':None, 'facility_name':None, 'facility_type':None, 'version_id':None } 
                           }
-        return(big_keyword_dict[type])
+        self.context_info=big_keyword_dict[type]
     
+    def get_model_name(self, type):
+        model_name_dict={'instrument_host':build.models.Instrument_Host}
+        self.model_name=model_name_dict[type]
+
     def fetch_context_info(self):
-        '''Fetches context information, given a context product's 
-        logical identifier.'''
+        '''Queries the API for info to get a Context_Product object's context
+         information.'''
         
         url = 'https://pds.nasa.gov/services/search/search?wt=json&q=product_class:Product_Context%20and%20data_class:'+self.type.capitalize()+'%20and%20identifier:'+self.lid
         #print(url)
@@ -55,22 +77,43 @@ class Context_Product:
 
         #excludes extra informational files (Resources) that aren't the actual context file
         doc=[d for d in data['response']['docs'] if d['data_product_type']!=['Resource'] ] [0]
-        print(doc.keys())
-        print()
+        #print(doc.keys())
+        #print()
         for key in self.context_info:
 
             try: #try to find the keyword in the xml file
                 self.context_info[key]=doc[key] #often a string, but sometimes a list 
             except KeyError: 
-                print('\nkeyword '+key +' not found in xml label for '+self.lid+', '+key+' value will be entered as "NONE"\n')
-                self.context_info[key]='NONE'
+                print('\nkeyword '+key +' not found in xml label for '+self.lid+', '+key+' value will be left as None \n')
+                #print(doc.keys())
+                #self.context_info[key]='N/A'
 
-        print(self.context_info)
+        #print(self.context_info)
         
     
     def insert_into_db(self):
-        if not all(self.context_info.values()):
-            raise ValueError('Context info missing/None, try calling fetch_context_info first.')
+        '''work in progress. Last 2 commented lines will successfully insert a
+        new row into the db and fill it with the info from the API query (yay)!
+        (only for instrument_host because that's what I've been testing with
+        today). Next step will be generalizing that to work with other types,
+        probably by mapping keywords yet again.
+        
+        Possibly more pressing is the issue of whether or not we need to change
+        the database structure at all- as it is now, references to other 
+        context products aren't saved in the same place as the rest of a 
+        product's info. That may not actually be a problem, but changing up 
+        that backend structure to make the user experience easier when adding
+        context is something worth exploring. And should probably be explored
+        before I fill the db with a bunch of new stuff, since I'll probably
+        have to redo a bunch of it if the table structure changes.'''
+        
+        #if not all(self.context_info.values()):
+        #    raise ValueError('Context info missing/None, try calling fetch_context_info first.')
+        
+        print(self.lid)
+        #print(self.context_info['identifier'], self.context_info['instrument_host_name'], self.context_info['instrument_host_type'], self.context_info['version_id'])
+        #new_product=self.model_name(lid=self.context_info['identifier'], name=self.context_info['instrument_host_name'][0], type_of=self.context_info['instrument_host_type'][0], vid=self.context_info['version_id'][0])
+        #new_product.save()
         
 
 
@@ -93,6 +136,16 @@ def test():
         print(doc['data_product_type'])       
 
 
-test_id = Context_Product(fetch_ids('Instrument_Host')[1]) 
-print(test_id.lid)
-test_id.fetch_context_info()
+
+type='Instrument_Host'
+
+for i in fetch_ids(type):
+    #print(i)
+    test_id = Context_Product(i) 
+    #print(test_id.lid)
+    in_db = test_id.model_name.objects.filter(lid=test_id.lid).exists()
+    if not in_db: #tangent, but could probably also filter out anything that's None in the db in a similar way
+        test_id.fetch_context_info()
+        test_id.insert_into_db()
+        break
+
