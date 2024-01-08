e547b063efabd473d20b06cbb30931fdc5a82710
sajomont <sajomont@atmos2023.nmsu.edu>
2024-01-08 11:40:23 -0700

    Added re_path changes to upgrade from Django 3.0 to 3.1

--- build/urls.py
+++ build/urls.py
@@ -1,7 +1,7 @@
 # Stdlib imports
 
 # Core Django imports
-from django.conf.urls import url, include
+from django.conf.urls import re_path, include
 
 # Third-party app imports
 
@@ -13,95 +13,95 @@ from . import views
 app_name='build'
 urlpatterns = [
     # Alias
-#    url(r'^(?P<pk_bundle>\d+)/alias/$', views.alias, name='alias'),
-    url(r'^(?P<pk_bundle>\d+)/alias_edit/(?P<pk_alias>[-\w]+)/$', views.alias_edit, name='alias_edit'),
+#    re_path(r'^(?P<pk_bundle>\d+)/alias/$', views.alias, name='alias'),
+    re_path(r'^(?P<pk_bundle>\d+)/alias_edit/(?P<pk_alias>[-\w]+)/$', views.alias_edit, name='alias_edit'),
 
-    url(r'^(?P<pk_bundle>\d+)/alias_delete/(?P<pk_alias>[-\w]+)/$', views.alias_delete, name='alias_delete'),
+    re_path(r'^(?P<pk_bundle>\d+)/alias_delete/(?P<pk_alias>[-\w]+)/$', views.alias_delete, name='alias_delete'),
 
     # Alias_Delete
-    url(r'^(?P<pk_bundle>\d+)/(?P<pk_alias>[-\w]+)/alias_delete/$', views.alias_delete, name='alias_delete'),
+    re_path(r'^(?P<pk_bundle>\d+)/(?P<pk_alias>[-\w]+)/alias_delete/$', views.alias_delete, name='alias_delete'),
 
     #Citation Information Delete
-    url(r'^(?P<pk_bundle>\d+)/(?P<pk_citation_information>[-\w]+)/citation_delete/$', views.delete_citation_information, name='citation_information_delete'),
+    re_path(r'^(?P<pk_bundle>\d+)/(?P<pk_citation_information>[-\w]+)/citation_delete/$', views.delete_citation_information, name='citation_information_delete'),
 
     #Modification History Delete
-    url(r'^(?P<pk_bundle>\d+)/(?P<pk_modification_history>[-\w]+)/modification_history/$', views.delete_modification_history, name='modification_history_delete'),
+    re_path(r'^(?P<pk_bundle>\d+)/(?P<pk_modification_history>[-\w]+)/modification_history/$', views.delete_modification_history, name='modification_history_delete'),
 
     # Build
-    url(r'^$', views.build, name='build'),
-    url(r'^(?P<bundle>\d+)/data_prep/$', views.data_prep, name='data_prep'),
+    re_path(r'^$', views.build, name='build'),
+    re_path(r'^(?P<bundle>\d+)/data_prep/$', views.data_prep, name='data_prep'),
 
     # Bundle
-    url(r'^(?P<pk_bundle>\d+)/$', views.bundle, name='bundle'), # Secure
-    url(r'^(?P<pk_bundle>\d+)/confirm_delete/$', views.bundle_delete, name='bundle_delete'), # Secure
-    url(r'^(?P<pk_bundle>\d+)/download/$', views.bundle_download, name='bundle_download'), # Need to secure.
-    url(r'^success_delete/$', views.success_delete, name='bundle_delete'),
+    re_path(r'^(?P<pk_bundle>\d+)/$', views.bundle, name='bundle'), # Secure
+    re_path(r'^(?P<pk_bundle>\d+)/confirm_delete/$', views.bundle_delete, name='bundle_delete'), # Secure
+    re_path(r'^(?P<pk_bundle>\d+)/download/$', views.bundle_download, name='bundle_download'), # Need to secure.
+    re_path(r'^success_delete/$', views.success_delete, name='bundle_delete'),
 
     # Citation_Information
-    url(r'^(?P<pk_bundle>\d+)/citation_information/$', views.citation_information, name='citation_information'),
+    re_path(r'^(?P<pk_bundle>\d+)/citation_information/$', views.citation_information, name='citation_information'),
     # Modification_History
-    url(r'^(?P<pk_bundle>\d+)/modification_story/$', views.modification_history, name='modification_history'),
+    re_path(r'^(?P<pk_bundle>\d+)/modification_story/$', views.modification_history, name='modification_history'),
     # Collections
 
 
     # Context
-    url(r'^$', views.context, name='context'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/$', views.context_search, name='context_search'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/$', views.context_search_investigation, name='context_search_investigation'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/instrument_host_or_facility/$', views.context_search_instrument_host_and_facility, name='context_search_instrument_host_and_facility'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/target/$', views.context_search_target_inv, name='context_search_target_investigation'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/instrument_host/(?P<pk_instrument_host>\d+)/instrument/$', views.context_search_instrument, name='context_search_instrument'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/facility/$', views.context_search_facility, name='context_search_facility'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/facility/(?P<pk_facility>\d+)/instrument/$', views.context_search_facility_instrument, name='context_search_facility_instrument'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/facility/(?P<pk_facility>\d+)/telescope/$', views.context_search_telescope, name='context_search_telescope'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/facility/(?P<pk_facility>\d+)/instrument_target/$', views.context_search_target_and_instrument, name='context_search_target_and_instrument'),
-    url(r'^(?P<pk_bundle>\d+)/contextsearch/target/$', views.context_search_target, name='context_search_target'),
+    re_path(r'^$', views.context, name='context'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/$', views.context_search, name='context_search'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/$', views.context_search_investigation, name='context_search_investigation'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/instrument_host_or_facility/$', views.context_search_instrument_host_and_facility, name='context_search_instrument_host_and_facility'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/target/$', views.context_search_target_inv, name='context_search_target_investigation'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/instrument_host/(?P<pk_instrument_host>\d+)/instrument/$', views.context_search_instrument, name='context_search_instrument'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/facility/$', views.context_search_facility, name='context_search_facility'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/facility/(?P<pk_facility>\d+)/instrument/$', views.context_search_facility_instrument, name='context_search_facility_instrument'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/facility/(?P<pk_facility>\d+)/telescope/$', views.context_search_telescope, name='context_search_telescope'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/investigation/(?P<pk_investigation>\d+)/facility/(?P<pk_facility>\d+)/instrument_target/$', views.context_search_target_and_instrument, name='context_search_target_and_instrument'),
+    re_path(r'^(?P<pk_bundle>\d+)/contextsearch/target/$', views.context_search_target, name='context_search_target'),
 
-    url(r'^(?P<pk_bundle>\d+)/context/(?P<pk_target>\d+)/delete_target/$', views.delete_target, name='delete_target'),
-    url(r'^(?P<pk_bundle>\d+)/context/(?P<pk_instrument>\d+)/delete_instrument/$', views.delete_instrument, name='delete_instrument'),
-    url(r'^(?P<pk_bundle>\d+)/context/(?P<pk_instrument_host>\d+)/delete_instrument_host/$', views.delete_instrument_host, name='delete_instrument_host'),
-    url(r'^(?P<pk_bundle>\d+)/context/(?P<pk_facility>\d+)/delete_facility/$', views.delete_facility, name='delete_facility'),
-    url(r'^(?P<pk_bundle>\d+)/context/(?P<pk_investigation>\d+)/delete_investigation/$', views.delete_investigation, name='delete_investigation'),
+    re_path(r'^(?P<pk_bundle>\d+)/context/(?P<pk_target>\d+)/delete_target/$', views.delete_target, name='delete_target'),
+    re_path(r'^(?P<pk_bundle>\d+)/context/(?P<pk_instrument>\d+)/delete_instrument/$', views.delete_instrument, name='delete_instrument'),
+    re_path(r'^(?P<pk_bundle>\d+)/context/(?P<pk_instrument_host>\d+)/delete_instrument_host/$', views.delete_instrument_host, name='delete_instrument_host'),
+    re_path(r'^(?P<pk_bundle>\d+)/context/(?P<pk_facility>\d+)/delete_facility/$', views.delete_facility, name='delete_facility'),
+    re_path(r'^(?P<pk_bundle>\d+)/context/(?P<pk_investigation>\d+)/delete_investigation/$', views.delete_investigation, name='delete_investigation'),
 
-    url(r'^investigations/$', views.investigations, name='investigations'),
-    url(r'^instruments/$', views.instruments, name='instruments'),
-    url(r'^instrument_hosts/$', views.instrument_hosts, name='instrument_hosts'),
+    re_path(r'^investigations/$', views.investigations, name='investigations'),
+    re_path(r'^instruments/$', views.instruments, name='instruments'),
+    re_path(r'^instrument_hosts/$', views.instrument_hosts, name='instrument_hosts'),
 
 
 
     # Data
-#    url(r'^(?P<pk_bundle>\d+)/data/$', views.data, name='data'),
-    url(r'^(?P<pk_bundle>\d+)/data/(?P<pk_data>\d+)/$', views.data, name='data'),    
-    url(r'^(?P<pk_bundle>\d+)/data/(?P<pk_product_observational>\d+)/$', views.product_observational, name='product_observational'),
+#    re_path(r'^(?P<pk_bundle>\d+)/data/$', views.data, name='data'),
+    re_path(r'^(?P<pk_bundle>\d+)/data/(?P<pk_data>\d+)/$', views.data, name='data'),    
+    re_path(r'^(?P<pk_bundle>\d+)/data/(?P<pk_product_observational>\d+)/$', views.product_observational, name='product_observational'),
 
-    url(
+    re_path(
         r'^(?P<pk_bundle>\d+)/data/(?P<pk_data>\d+)/product_observational/(?P<pk_product_observational>\d+)/array/$',
         views.array,
         name='array'
     ),
 
 
-    url(r'^(?P<pk_bundle>\d+)/data/(?P<pk_data>\d+)/display_dictionary/(?P<pk_display_dictionary>\d+)/$', views.display_dictionary, name='display_dictionary'),
+    re_path(r'^(?P<pk_bundle>\d+)/data/(?P<pk_data>\d+)/display_dictionary/(?P<pk_display_dictionary>\d+)/$', views.display_dictionary, name='display_dictionary'),
 
 
 
 
-    url(r'^(?P<pk_bundle>\d+)/data/table/(?P<pk_product_observational>\d+)/$', views.table_detail, name='table_detail'),
-    url(r'^(?P<pk_bundle>\d+)/(?P<pk_data>\d+)/table_creation/$', views.Table_Creation, name='table_creation'),
-    url(r'^(?P<pk_bundle>\d+)/(?P<table>[-/w]+)/field_creation/$', views.Field_Creation, name='field_creation'),
+    re_path(r'^(?P<pk_bundle>\d+)/data/table/(?P<pk_product_observational>\d+)/$', views.table_detail, name='table_detail'),
+    re_path(r'^(?P<pk_bundle>\d+)/(?P<pk_data>\d+)/table_creation/$', views.Table_Creation, name='table_creation'),
+    re_path(r'^(?P<pk_bundle>\d+)/(?P<table>[-/w]+)/field_creation/$', views.Field_Creation, name='field_creation'),
 
     # Dictionary
-    url(r'^(?P<pk_bundle>\d+)/data/array/display_dictionary/$', views.display_dictionary, name='display_dictionary'),
+    re_path(r'^(?P<pk_bundle>\d+)/data/array/display_dictionary/$', views.display_dictionary, name='display_dictionary'),
     
     # Document
-    url(r'^(?P<pk_bundle>\d+)/document/$', views.document, name='document'),
-    url(r'^(?P<pk_bundle>\d+)/document/product_document/(?P<pk_product_document>\d+)/$', views.product_document, name='product_document'),
+    re_path(r'^(?P<pk_bundle>\d+)/document/$', views.document, name='document'),
+    re_path(r'^(?P<pk_bundle>\d+)/document/product_document/(?P<pk_product_document>\d+)/$', views.product_document, name='product_document'),
     
     # Directory 
-    url(r'^(?P<path>.*)/$', views.index, name='directory_view'),
+    re_path(r'^(?P<path>.*)/$', views.index, name='directory_view'),
 
 
-    # XML_Schema --> A view that no one sees.  So no xml_schema url.  This might even be removed 
+    # XML_Schema --> A view that no one sees.  So no xml_schema re_path.  This might even be removed 
     # completely from PDS4
 
 
