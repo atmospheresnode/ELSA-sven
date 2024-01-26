5446de98751407ba931b2a124c6cac243b17cc45
sajomont <sajomont@atmos2023.nmsu.edu>
2024-01-26 09:57:42 -0700

    local changes of offline before checking out frontend branch

--- main/urls.py
+++ main/urls.py
@@ -5,23 +5,23 @@ elsa.main.urls shows the listing of all current urls associated with elsa's main
 
 """
 
-from django.urls import re_path
+from django.conf.urls import url
 from . import views
 
 app_name='main'
 urlpatterns = [
 
     # elsa's main views.
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^about/$', views.about, name='about'),
-    re_path(r'^services/$', views.services, name='services'),
-    re_path(r'^construction/$', views.construction, name='construction'),
-    re_path(r'^contact/$', views.contact, name='contact'),
-    re_path(r'^error/$', views.error, name='error'),
-    re_path(r'^restricted_access/$', views.restricted_access, name='restricted_access'),
-    re_path(r'^simple_upload/$', views.simple_upload, name='simple_upload'),
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^services/$', views.services, name='services'),
+    url(r'^construction/$', views.construction, name='construction'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^error/$', views.error, name='error'),
+    url(r'^restricted_access/$', views.restricted_access, name='restricted_access'),
+    url(r'^simple_upload/$', views.simple_upload, name='simple_upload'),
 
 
     # I use this for development (k).
-    re_path(r'^success/$', views.success, name='success'),
+    url(r'^success/$', views.success, name='success'),
 ]
