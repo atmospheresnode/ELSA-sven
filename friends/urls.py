8758e7f5456174ec4ee06ac2129ed51eea7f7264
sajomont <sajomont@atmos2023.nmsu.edu>
2024-05-17 10:57:40 -0600

    Frontend changes to the XML display and url to re_path

--- friends/urls.py
+++ friends/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,15 +7,15 @@ from . import views
 app_name='friends'
 urlpatterns = [
     #url(r'^$', views.FriendList.as_view(), name='friends'),
-    url(r'^(?P<pk_user>\d+)/$', views.profile, name='profile'),
-    url(r'^login/$', views.friend_login, name='login'),
-    url(r'^logout/$', views.friend_logout, name='logout'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^(?P<pk_user>\d+)/settings/$', views.friend_settings, name='settings'),
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path(r'^(?P<pk_user>\d+)/$', views.profile, name='profile'),
+    re_path(r'^login/$', views.friend_login, name='login'),
+    re_path(r'^logout/$', views.friend_logout, name='logout'),
+    re_path(r'^register/$', views.register, name='register'),
+    re_path(r'^(?P<pk_user>\d+)/settings/$', views.friend_settings, name='settings'),
+    re_path(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    re_path(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    re_path(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
--- review/urls.py
+++ review/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 
 
 app_name='review'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    re_path(r'^$', views.index, name='index'),
 ]
