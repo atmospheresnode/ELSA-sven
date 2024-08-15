432af174eb7cadaf7d390f16ec5a6f3155ac3d0c
rupakdey <rupakdey@atmos2023.nmsu.edu>
2024-08-15 11:05:39 -0600

    Profile Page UI Changes

--- friends/urls.py
+++ friends/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     re_path(r'^logout/$', views.friend_logout, name='logout'),
     re_path(r'^register/$', views.register, name='register'),
     re_path(r'^(?P<pk_user>\d+)/settings/$', views.friend_settings, name='settings'),
+    re_path(r'^useraccount/$', views.friend_useraccount, name='useraccount'),
     re_path(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
     re_path(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
