c5f2da9fa62082ea6468aa7883e31e45830a0aa2
rupakdey <rupakdey@atmos2023.nmsu.edu>
2025-07-25 05:53:27 -0600

    online save draft issue

--- review/urls.py
+++ review/urls.py
@@ -5,6 +5,6 @@ app_name = 'review'
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^save-draft/(?P<draft_id>[0-9a-fA-F\-]{36})/$', views.save_draft, name='save_draft'),
+    re_path(r'^save-draft/(?P<draft_id>[0-9a-fA-F\-]{36})/?$', views.save_draft, name='save_draft'),
     re_path(r'^load-draft/(?P<draft_id>[0-9a-fA-F\-]{36})/$', views.load_draft, name='load_draft'),
 ]
