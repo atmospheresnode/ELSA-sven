d2f95519777d2c6fdfb087e969ffaffb697c0761
sajomont <sajomont@atmos2023.nmsu.edu>
2026-02-27 10:37:10 -0700

    Fixes to uploader processing xml code

--- build/views.py
+++ build/views.py
@@ -3761,7 +3761,7 @@ def variable_coord_to_product(bundle):
     # Include Sub-Directories
     # =========================================================================================
     # 1. Set the top-level working directory
-    working_dir = os.path.join(settings.ARCHIVE_DIR, 'netcdf')
+    working_dir = os.path.join(settings.BASE_DIR, 'uploads')
 
     # 2. Recursively find all .nc files
     os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
@@ -3938,3 +3938,7 @@ def variable_coord_to_product(bundle):
             # Close appropriate label(s)
             print(' ... Closing Label ... ')
             close_label(output_path, label_root, label_list[2])
+
+        # Temporary remove netcdf file before figuring out how to move it
+        os.remove(nc_path)
+        print('removed: ' + nc_path)
