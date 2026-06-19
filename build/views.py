2dcb4b2cc1917346b02ef8956c66e471b9de8d71
nmille <nmille@atmos2023.nmsu.edu>
2026-06-19 11:19:57 -0600

    small bug fixes

--- build/views.py
+++ build/views.py
@@ -956,8 +956,7 @@ def bundle(request, pk_bundle):
 
             context_dict['context_successful_submit'] = True
 
-            return render(request, 'build/bundle/bundle.html', context_dict)
-            # return render(request, 'build/bundle/bundle.html', context_dict)
+            return HttpResponseRedirect('/elsa/build/' + pk_bundle + '/')
 
 
         # After ELSAs friend hits submit, if the forms are completed correctly, we should enter
